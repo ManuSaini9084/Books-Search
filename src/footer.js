3fc2656296a68f87dc9a19d4635cfb425afaa243
Manu Saini <139550825+ManuSaini9084@users.noreply.github.com>
2024-05-26 11:59:39 +0530

    Add files via upload

---
+++ src/footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <h4>
+          &#60;&#47;&#62; with ❤️ & ☕ by{" "}
+          <a
+            style={{
+              textDecoration: "underline",
+              color: "white",
+            }}
+            href="https://github.com/ManuSaini9084"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Manu Saini
+          </a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
